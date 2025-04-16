76134c37dcca03f47142f074ba5c64529fa7a931
Alexey Po <flexxus@mail.ru>
2025-04-16 16:31:12 +0300

    small fixes in widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,9 +1,8 @@
-from src.masks import get_mask_card_number
-from src.masks import get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(user_account: str) -> str:
-    """ Функция принимает номер карты или счет и возвращает в замаскированном виде"""
+    """ Функция принимает номер карты или счета и возвращает в замаскированном виде"""
 
     splited_user_account = user_account.split()
     if splited_user_account[0] == "Счет":
@@ -13,6 +12,8 @@ def mask_account_card(user_account: str) -> str:
 
 
 def get_date(date_and_info: str) -> str:
+    """ Функция возвращае дату  в формате ДД.ММ.ГГГГ"""
+
     info_splited = date_and_info.split("T")
     date_splited = info_splited[0].split("-")
     date = f"{date_splited[2]}.{date_splited[1]}.{date_splited[0]}"
