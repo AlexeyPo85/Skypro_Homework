f5303398c84e6ac95662e30c6721546bf23f3d23
Alexey Po <flexxus@mail.ru>
2025-04-16 14:56:46 +0300

    original code after homework 9.1

---
+++ src/masks.py
@@ -0,0 +1,10 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция возвращает маску номера карты, видны первые 6 и последние 4 цифры"""
+
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
+
+def get_mask_account(account: str) -> str:
+    """Функция возвращает маску номера счета в которй видны последние 4 цифры"""
+
+    return f"**{account[-4:]}"
