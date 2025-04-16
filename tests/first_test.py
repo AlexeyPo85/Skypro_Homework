85b139a335b883c6b20439628f942a25d9725be4
Alexey Po <flexxus@mail.ru>
2025-04-16 15:48:43 +0300

    fixes widget.py and add code for testing in first_test.py

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,4 +1,14 @@
 from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card
+
+user_input = "Maestro 1596837868705199"
+user_input_2 = "Счет 35383033474447895560"
+user_input_3 = "Visa Platinum 8990922113665229"
 
 print(get_mask_card_number("7000792289606361"))
 print(get_mask_account("73654108430135874305"))
+print()
+print(mask_account_card(user_input))
+print(mask_account_card(user_input_2))
+print(mask_account_card(user_input_3))
+
