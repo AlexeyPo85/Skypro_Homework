229c0dd8d3121664c8912326bd12810c1a98bc69
Alexey Po <flexxus@mail.ru>
2025-04-24 12:13:26 +0300

    fixes processing.py and end of homework 10.1

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(list_of_dicts: list[dict], state_value='EXECUTED') -> list[dict]:
+def filter_by_state(list_of_dicts: list[dict], state_value: str = 'EXECUTED') -> list[dict]:
     """ Функция возвращает отсортированный список по значению 'EXECUTED',
     или список со значением 'CANCELED', если передан аргумент 'CANCELED'.
     """
@@ -10,7 +10,7 @@ def filter_by_state(list_of_dicts: list[dict], state_value='EXECUTED') -> list[d
                 filtered_list.append(dict_data)
     else:
         for dict_data in list_of_dicts:
-            if dict_data['state'] == 'CANCELED':
+            if dict_data['state'] == state_value:
                 filtered_list.append(dict_data)
 
     return filtered_list
@@ -21,18 +21,16 @@ def filter_by_state(list_of_dicts: list[dict], state_value='EXECUTED') -> list[d
     #     filtered_list = [dict_data for dict_data in list_of_dicts if dict_data['state'] == 'EXECUTED']
     #     return filtered_list
     #
-    # if state_value == 'CANCELED':
-    #     filtered_list = [dict_data for dict_data in list_of_dicts if dict_data['state'] != 'EXECUTED']
+    # else:
+    #     filtered_list = [dict_data for dict_data in list_of_dicts if dict_data['state'] == state_value]
     #     return filtered_list
 
 
-def sort_by_date(list_of_dicts: list[dict], sort_arg='True') -> list[dict]:
-    """ Функция возвращает отсортированный по дате список(по убыванию)."""
-    # В задании не понятно в переменной булево значение или строка,
-    # там написано 'True' именно в кавычках, я понял что в переменной строковое значение.
+def sort_by_date(list_of_dicts: list[dict], sort_arg: bool = True) -> list[dict]:
+    """ Функция возвращает отсортированный по дате список(по умолчанию-по убыванию,
+    или по возрастанию, если в параметр со значением по умолчанию введено False)."""
 
-    sorted_list = []
-    if sort_arg == 'True':
+    if sort_arg:
         sorted_list = sorted(list_of_dicts, key=lambda d: d['date'], reverse=True)
     else:
         sorted_list = sorted(list_of_dicts, key=lambda d: d['date'])
