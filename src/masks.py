f53dec7d6b078c80183a8866a622c9f6368e7206
Alexey Po <flexxus@mail.ru>
2025-05-01 13:29:37 +0300

    add test_get_mask_account

--- src/masks.py
+++ src/masks.py
@@ -7,10 +7,22 @@ def get_mask_card_number(card_number: str) -> str:
     if card_number.isdigit() == False:
         return 'Введён не корректный номер карты'
 
+    if isinstance(card_number, str) == False:
+        return 'Введён не корректный номер карты'
+
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
 def get_mask_account(account: str) -> str:
-    """Функция возвращает маску номера счета в которй видны последние 4 цифры"""
+    """Функция возвращает маску номера счета в которой видны последние 4 цифры"""
+
+    if len(account) != 20:
+        return 'Введён не корректный номер счёта'
+
+    if account.isdigit() == False:
+        return 'Введён не корректный номер счёта'
+
+    if isinstance(account, str) == False:
+        return 'Введён не корректный номер счёта'
 
     return f"**{account[-4:]}"
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,6 +1,7 @@
 import pytest
 
 from src.masks import get_mask_card_number
+from src.masks import get_mask_account
 
 
 def test_get_mask_card_number(card_number):
@@ -12,6 +13,7 @@ def test_get_mask_card_number(card_number):
                                               ('1', 'Введён не корректный номер карты'),
                                               ('abcdefghijklmnop', 'Введён не корректный номер карты'),
                                               ('ccc', 'Введён не корректный номер карты'),
+                                              ([1, 2, 3], 'Введён не корректный номер карты'),
                                               ])
 def test_get_mask_card_number_2(card_num, result):
     assert get_mask_card_number(card_num) == result
@@ -21,3 +23,17 @@ def test_get_mask_card_number_3():
     assert get_mask_card_number('') == 'Введён не корректный номер карты'
 
 
+def test_get_mask_account(account_number):
+    assert get_mask_account(account_number) == '**4305'
+
+
+@pytest.mark.parametrize('account_num, result', [('64686473678894779589', '**9589'),
+                                                 ('00000000000000000000', '**0000'),
+                                                 ('123', 'Введён не корректный номер счёта'),
+                                                 ('abcdefghijklmnopqrst', 'Введён не корректный номер счёта'),
+                                                 ('abc', 'Введён не корректный номер счёта'),
+                                                 ('', 'Введён не корректный номер счёта'),
+                                                 ([1, 2, 3], 'Введён не корректный номер счёта'),
+                                                 ])
+def test_get_mask_account_2(account_num, result):
+    assert get_mask_account(account_num) == result
