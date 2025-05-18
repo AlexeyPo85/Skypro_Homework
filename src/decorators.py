b03f29b853c5193ea3869da2bc3c9d51f4ed6851
Alexey Po <flexxus@mail.ru>
2025-05-18 15:50:43 +0300

    start homework 11.2, add decorator log in decorators.py

---
+++ src/decorators.py
@@ -0,0 +1,20 @@
+def log(filename):
+    def wrapper(func):
+        def inner(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                if filename:
+                    with open(filename, 'w', encoding='utf-8') as file:
+                        file.write(f'Функция {func.__name__}, вызвана с аргументами {args}, {kwargs} выполнена успешно. '
+                                   f'Результат: {result}')
+                else:
+                    print(f'Функция {func.__name__}, вызвана с аргументами {args}, {kwargs} выполнена успешно. '
+                                   f'Результат: {result}')
+            except Exception as e:
+                if filename:
+                    with open(filename, 'w', encoding='utf-8') as file:
+                        file.write(f'Функция {func.__name__} вывела ошибку:{e}. Входные данные:{args}, {kwargs}')
+                else:
+                    print(f'Функция {func.__name__} вывела ошибку:{e}. Входные данные:{args}, {kwargs}')
+        return inner
+    return wrapper
