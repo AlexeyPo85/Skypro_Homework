46404d15899c35b039e4657e8f4f82a255bdf1d3
Alexey Po <flexxus@mail.ru>
2025-06-06 21:30:28 +0300

    add .env.example and tests in test_external_api.py

--- src/external_api.py
+++ src/external_api.py
@@ -8,11 +8,16 @@ load_dotenv()
 def convert_to_rub(code: str, amount: str) -> float:
     " Функция конвертирует сумму в RUB по текущему курсу."
 
-    url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={code}&amount={amount}"
-    headers = {"apikey": os.getenv("API_KEY")}
-    payload = {}
-    response = requests.request("GET", url, headers=headers, data = payload)
-    status_code = response.status_code
-    result = response.json()
-    result_amount = result["result"]
-    return result_amount
+    if code != "USD" or code != "EUR" or float(amount) != float:
+        return {}
+    else:
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={code}&amount={amount}"
+        headers = {"apikey": os.getenv("API_KEY")}
+        payload = {}
+        response = requests.get(url, headers=headers, data = payload)
+        status_code = response.status_code
+        result = response.json()
+        result_amount = result["result"]
+        return result_amount
+
+print(convert_to_rub("USD", "a"))
