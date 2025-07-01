1283691f1294fe1cd4c80e03627f36c24438bf96
Alexey Po <flexxus@mail.ru>
2025-07-01 20:22:49 +0300

    add process_bank_operations in search_process.py

--- src/search_process.py
+++ src/search_process.py
@@ -1,8 +1,10 @@
 import re
+from collections import Counter
 
 
 def process_bank_search(data:list[dict], search:str)->list[dict]:
     """Функция находит операции в списке банковских операций по заданной строке"""
+
     result = []
     for operation in data:
         if re.search(search, operation["description"]):
@@ -10,3 +12,13 @@ def process_bank_search(data:list[dict], search:str)->list[dict]:
         else:
             continue
     return result
+
+
+def process_bank_operations(data:list[dict], categories:list)->dict:
+    """Функция принимает список словарей банковских операций,
+    возвращает словарь, в котором ключи — это названия категорий,
+    а значения — это количество операций в каждой категории"""
+
+    list_operations = [operation["description"] for operation in data if operation["description"] in categories]
+    count_operations = Counter(list_operations)
+    return count_operations
