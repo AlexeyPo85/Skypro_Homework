933d24b6e1711c0fff48f13a5dabb5472e2d34f5
Alexey Po <flexxus@mail.ru>
2025-07-01 20:56:44 +0300

    add tests in test_search_process.py

--- src/search_process.py
+++ src/search_process.py
@@ -5,13 +5,17 @@ from collections import Counter
 def process_bank_search(data:list[dict], search:str)->list[dict]:
     """Функция находит операции в списке банковских операций по заданной строке"""
 
-    result = []
-    for operation in data:
-        if re.search(search, operation["description"]):
-            result.append(operation)
-        else:
-            continue
-    return result
+    try:
+        result = []
+        for operation in data:
+            if search:
+                if re.search(search, operation["description"]):
+                    result.append(operation)
+                else:
+                    continue
+        return result
+    except:
+        return []
 
 
 def process_bank_operations(data:list[dict], categories:list)->dict:
@@ -19,6 +23,9 @@ def process_bank_operations(data:list[dict], categories:list)->dict:
     возвращает словарь, в котором ключи — это названия категорий,
     а значения — это количество операций в каждой категории"""
 
-    list_operations = [operation["description"] for operation in data if operation["description"] in categories]
-    count_operations = Counter(list_operations)
-    return count_operations
+    try:
+        list_operations = [operation["description"] for operation in data if operation["description"] in categories]
+        count_operations = Counter(list_operations)
+        return count_operations
+    except:
+        return {}
